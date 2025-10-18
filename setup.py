9f203d4677cd2d1af39535f31078aabc7dc6b3a9
wikachu <wikabsd@gmail.com>
2025-10-18 16:07:26 +0700

    Web App with RAG Integrated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Wahyu Dhia Satwika",
+    author_email="wahyu.dhiasatwika@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
